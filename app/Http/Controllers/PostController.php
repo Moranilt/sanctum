b34f6be6f3fd018b443b13494ef4770e208fb4d4
Danil <moranilt@yahoo.com>
2020-10-22 12:24:13 +0300

    Update Posts
    Updating Post and deleting.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Post;
+use App\PostsViews;
 
 class PostController extends Controller
 {
@@ -35,7 +35,35 @@ class PostController extends Controller
         $post->body = $data['body'];
         $post->save();
         $post->addCategory($data['categories']);
+        $views = new PostsViews();
+        $post->addViews($views);
+        return redirect()->route('home');
+    }
 
+    public function edit(Post $post)
+    {
+        return view('post.edit', ['post' => $post]);
+    }
+
+    public function update(Post $post, Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'categories' => 'required'
+        ]);
+        $post->slug = Str::slug($data['title'], '-');
+        $post->excerpt = Str::limit($data['body'], 40, '...');
+        $post->title = $data['title'];
+        $post->body = $data['body'];
+        $post->update();
+        $post->addCategory($data['categories']);
+        return redirect()->route('post.show', $post->slug);
+    }
+
+    public function delete(Post $post)
+    {
+        $post->delete();
         return redirect()->route('home');
     }
 }
