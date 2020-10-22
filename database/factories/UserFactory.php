e79d1bb234f284fcd599a63943a3a527ce737aed
Danil <moranilt@yahoo.com>
2020-10-22 12:28:01 +0300

    Some seeder changes
    Added some new seeders.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,9 +18,12 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $name = $faker->name;
+    $slug = Str::slug($name, '-');
     return [
-        'name' => $faker->name,
+        'name' => $name,
         'email' => $faker->unique()->safeEmail,
+        'slug' => $slug,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
