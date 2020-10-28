e9c101b40ef3f60df44af5c3d1e1fd8bd7b22d54
Danil <moranilt@yahoo.com>
2020-10-28 22:24:48 +0300

    Deleted dump code
    After updating comments with Vue js this code became useless.

--- public/js/main.js
+++ public/js/main.js
@@ -3,51 +3,6 @@
 (function($) {
 
 
-
-let commentReply = `
-<form class="post-reply comment-reply" action="{{route('comment.store', $post->slug)}}">
-@csrf
-<input type="hidden" name="parent_id" value="{{$comment->id}}">
-<div class="row">
-    <div class="col-md-12">
-        <div class="form-group">
-            <textarea class="input" name="message" placeholder="Message"></textarea>
-        </div>
-    </div>
-
-    <div class="col-md-12">
-        <button class="primary-button">Submit</button>
-    </div>
-
-</div>
-</form>
-`
-$('.reply').click(function(e){
-    e.preventDefault()
-    let comment_id = $(this).parent().data('comment-id')
-    let route = $(this).parent().data('route')
-    let token = $(this).parent().data('token')
-    console.log(route)
-    $(this).after(`
-    <form class="post-reply comment-reply" action="`+route+`">
-    <input type="hidden" name="_token" value="`+token+`">
-    <input type="hidden" name="parent_id" value="`+comment_id+`">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <textarea class="input" name="message" placeholder="Message"></textarea>
-            </div>
-        </div>
-
-        <div class="col-md-12">
-            <button class="primary-button">Submit</button>
-        </div>
-
-    </div>
-    </form>
-    `)
-})
-
 	// Mobile dropdown
 	$('.has-dropdown>a').on('click', function() {
 		$(this).parent().toggleClass('active');
@@ -55,20 +10,6 @@ $('.reply').click(function(e){
 
 
 
-	// // Aside Nav
-	// $(document).click(function(event) {
-	// 	if (!$(event.target).closest($('#nav-aside')).length) {
-	// 		if ( $('#nav-aside').hasClass('active') ) {
-	// 			$('#nav-aside').removeClass('active');
-	// 			$('#nav').removeClass('shadow-active');
-	// 		} else {
-	// 			if ($(event.target).closest('#aside-btn').length) {
-	// 				$('#nav-aside').addClass('active');
-	// 				$('#nav').addClass('shadow-active');
-	// 			}
-	// 		}
-	// 	}
-	// });
 
 	$('.nav-aside-close').on('click', function () {
 		$('#nav-aside').removeClass('active');
